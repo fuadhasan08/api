23163fc010270c748cb8ac5c175f7ebeb17b08d1
fuadhasan08 <fuadhasandipro08@gmail.com>
2023-09-05 21:43:50 +0600

    modification

---
+++ app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+// import memberRoute from './routes/member.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// app.use('/api/members', memberRoute);
+
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PW || '',
+  database: process.env.MYSQL_DB,
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.log('Error connecting to MySQL: ' + err);
+  } else {
+    console.log('Connected to MySQL!');
+    connection.release();
+  }
+});
+
+app.get('/', (req, res) => {
+  pool.query('SELECT * FROM services', function (err, rows, fields) {
+    if (err) {
+      res.send(500).json({ error: 'Error' });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+app.listen(8000, () => {
+  // console.log(`App is Listening to port 8000`);
+});
